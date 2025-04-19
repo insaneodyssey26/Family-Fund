a3e79f225cd9fd7ad4a40eb235318d63801677af
Masum <hydrapikachu29@gmail.com>
2025-04-19 17:48:11 +0530

    fixed tab animations

--- src/pages/RegisterResource.tsx
+++ src/pages/RegisterResource.tsx
@@ -1,1 +1,17 @@
- 
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styles from '../styles/RegisterResource.module.css';
+
+const RegisterResource: React.FC = () => {
+    const { resourceId } = useParams<{ resourceId: string }>();
+
+    return (
+        <div className={styles.container}>
+            <h1>Register Resource</h1>
+            <p>Resource ID: {resourceId}</p>
+            {/* Add your resource registration form here */}
+        </div>
+    );
+};
+
+export default RegisterResource; 
